6b8b0c45d32935a279239e1133df653b320aa597
Daniel Esterhuizen <DanielE@fuzzy-logic.co.za>
2016-01-30 00:30:29 +0200

    Sounds! Buildings!

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+	public AudioClip[] Screams;
+	public AudioClip[] Blessings;
+	public AudioClip[] Curses;
+	public AudioSource source;
+
+	static AudioManager instance;
+
+
+	public void Start()
+	{
+		instance = this;
+	}
+
+
+	public void PlayCurse () 
+	{
+		source.clip = Curses[(int) Random.Range(0,Curses.Length)];
+		source.Play();
+	}
+
+
+	public void PlayScream () 
+	{
+		source.clip = Screams[(int) Random.Range(0,Screams.Length)];
+		source.Play();
+	}
+
+	public void PlayBlessing () 
+	{
+		source.clip = Blessings[(int) Random.Range(0,Blessings.Length)];
+		source.Play();
+	}
+
+
+	void Update()
+
+	{
+
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			PlayScream();
+		}
+	
+	
+		if (Input.GetKeyDown(KeyCode.S))
+		{
+			PlayBlessing();
+		}
+
+		if (Input.GetKeyDown(KeyCode.D))
+		{
+			PlayCurse();
+		}
+	}
+
+}
