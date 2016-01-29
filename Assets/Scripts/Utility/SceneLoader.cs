35e9b006ee4055c7c801f1e698baf775cc83a0e8
Luan Joubert <Luan@fuzzy-logic.co.za>
2016-01-30 01:38:44 +0200

    Main Menu phase 1

--- Assets/Scripts/Utility/SceneLoader.cs
+++ Assets/Scripts/Utility/SceneLoader.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class SceneLoader : MonoBehaviour 
 {
-
-	// Use this for initialization
 	public void LoadScene (int level) 
 	{
 		Application.LoadLevel(level);
 	}
 
+	public void QuitApplication()
+	{
+		Application.Quit ();
+	}
 }
