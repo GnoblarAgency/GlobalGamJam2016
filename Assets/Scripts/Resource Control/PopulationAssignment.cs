901cc99932282babe0062dc01b6b88cbc9574065
Daniel Esterhuizen <DanielE@fuzzy-logic.co.za>
2016-01-30 09:10:14 +0200

    Added population assignment script

---
+++ Assets/Scripts/Resource Control/PopulationAssignment.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopulationAssignment : MonoBehaviour {
+
+
+	private ResourceModifier foodCollecting;
+	private ResourceModifier prisonerCollecting;
+
+	public float foodAssignment = 0.5f;
+	public float prisonerAssignment = 0.5f;
+
+	public float foodClamp = 100;
+	public float prisonerClamp = 80;
+
+	// Use this for initialization
+	void Start () 
+	{
+		foodCollecting = new ResourceModifier(ResourceNames.FOOD, DetermineFoodModifier());
+		prisonerCollecting = new ResourceModifier(ResourceNames.PRISONERS, DetermineFoodModifier());
+	}
+	
+	public float DetermineFoodModifier()
+	{
+		return (ResourcesManager.instance.GetResourcePopulation().TotalAmount*foodAssignment)/prisonerClamp;
+	}
+
+
+	public float DeterminePrisonerModifier()
+	{
+		return (ResourcesManager.instance.GetResourcePopulation().TotalAmount*prisonerAssignment)/foodClamp;
+	}
+
+	public void ApplyModifiers()
+	{
+		ResourcesManager.instance.ApplyModifier(foodCollecting);
+		ResourcesManager.instance.ApplyModifier(prisonerCollecting);
+	}
+}
