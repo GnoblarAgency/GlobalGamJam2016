3209b420b63e951e6a88464c6da9d664a5939ebd
Con Momberg <con@fuzzy-logic.co.za>
2016-01-30 02:10:07 +0200

    Chicomecoatl is unhappy

--- Assets/Scripts/Buildings/BuildingManager.cs
+++ Assets/Scripts/Buildings/BuildingManager.cs
@@ -1,15 +1,62 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BuildingManager : MonoBehaviour {
+public class BuildingManager : MonoBehaviour
+{
+	#region PROPERTIES
+	public static BuildingManager Instance { get; private set; }
+	#endregion
 
-	// Use this for initialization
-	void Start () {
-	
+
+	#region PUBLIC VARIABLES
+	#endregion
+
+
+	#region PRIVATE VARIABLES
+	#endregion
+
+
+	#region UNITY EVENTS
+	void Awake ()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		else
+		{
+			Debug.LogError ("There is more than one BuildingManager in the scene!");
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update () 
+	{
+		if (Input.GetMouseButtonUp (0))
+		{
+			SelectBuilding (new Vector2 (Input.mousePosition.x, Input.mousePosition.y));
+		}
 	}
+	#endregion
+
+
+	#region PUBLIC API
+
+	#endregion
+
+
+	#region HELPER FUNCTIONS
+	void SelectBuilding (Vector2 position)
+	{
+		if (true) //TODO replace with UI not already displaying some UI
+		{
+			Ray ray = Camera.main.ScreenPointToRay(position);
+			RaycastHit hit = new RaycastHit();
+
+			if (Physics.Raycast(ray, out hit, 100, Layers.BUILDINGS_MASK))
+			{
+				
+			}
+		}
+	}
+	#endregion
 }
