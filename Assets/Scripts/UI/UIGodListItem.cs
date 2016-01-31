1b84beb8da8f07e73e64b20427ef78710ba33e7a
Con Momberg <con@fuzzy-logic.co.za>
2016-01-31 12:58:26 +0200

    Gods and UI

---
+++ Assets/Scripts/UI/UIGodListItem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIGodListItem : MonoBehaviour
+{
+	#region PUBLIC VARIABLES
+	public Image godIcon;
+	public Scrollbar favourBar;
+	#endregion
+
+
+	#region UNITY EVENTS
+	#endregion
+
+
+	#region PUBLIC API
+	public void SetDetails(Sprite image, float favour)
+	{
+		godIcon.sprite = image;
+		favourBar.value = favour;
+	}
+	#endregion
+}
